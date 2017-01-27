c767a1182ded227bd7039ef23a8b69689c417801
marquesin <nmarquesin@live.com>
2017-01-27 13:02:30 +0000

    Save old version of resumeBuilder.js, clean current version and create projects array

--- js/resumeBuilder_old.js
+++ js/resumeBuilder_old.js
@@ -1,17 +1,8 @@
-// ----------- Class Note
-
-// here is an object declared with bracket notation:
-// var myObj;
-// myObj["someProperty"] = someValue;
-
-// here is an object declared with dot notation:
-// var myObj;
-// myObj.someProperty = someValue;
-
-// -----------
-
-
-// create and append name and role in header
+// $("#main").append(["Nathalie Marquesin"]);
+//var awesomeThoughts = "I am Nathalie and I am awesome";
+//console.log(awesomeThoughts);
+//var funThoughts = awesomeThoughts.replace(["awesome"], ["fun"]);
+//$("#main").append([funThoughts]);
 
 var formattedName = HTMLheaderName.replace(["%data%"], ["Nathalie Marquesin"]);
 var formattedRole = HTMLheaderRole.replace(["%data%"], ["Web Developer"]);
@@ -19,7 +10,16 @@ var formattedRole = HTMLheaderRole.replace(["%data%"], ["Web Developer"]);
 $("#header").prepend([formattedRole]);
 $("#header").prepend([formattedName]);
 
-// create and append bio object
+//var name = ;
+//var role = ;
+//var contactInfo = ["nmarquesin@live.com", "0044 7496 529051", "@mrsricehill", "https://github.com/nmarquesin", "Newbury, UK"];
+//var picURL = "https://avatars.githubusercontent.com/u/23559837?v=3";
+//var welcomeMsg = "Welcome to my corner of the Web.";
+//var skills = ["Python", "HTML+CSS", "JavaScript", "ML"];
+
+
+// here is an object declared with object notation:
+// var myObj = {};
 
 var bio = {
   "name" : "Nathalie Marquesin",
@@ -36,6 +36,17 @@ var bio = {
   "skills" : ["Python", "HTML+CSS", "JavaScript", "ML"]
 };
 
+
+
+// here is an object declared with bracket notation:
+// var myObj;
+// myObj["someProperty"] = someValue;
+
+
+
+// -----------
+
+// format bio variables
 var formattedEmail = HTMLemail.replace(["%data%"], bio.contactInfo.email);
 var formattedMobile = HTMLmobile.replace(["%data%"], bio.contactInfo.mobile);
 var formattedGithub = HTMLgithub.replace(["%data%"], bio.contactInfo.github);
@@ -44,6 +55,7 @@ var formattedPic = HTMLbioPic.replace(["%data%"], bio.picURL);
 var formattedWelcomeMessage = HTMLwelcomeMsg.replace(["%data%"], bio.welcomeMsg);
 var formattedSkills = HTMLskills.replace(["%data%"], bio.skills);
 
+// append or prepend bio variables
 $("#topContacts").append([formattedMobile]);
 $("#topContacts").append([formattedEmail]);
 $("#footerContacts").append([formattedGithub]);
@@ -51,16 +63,27 @@ $("#topContacts").append([formattedLocation]);
 $("#header").append([formattedWelcomeMessage]);
 
 
-// create work object
 
-var work = { };
+// here is an object declared with dot notation:
+// var myObj;
+// myObj.someProperty = someValue;
+
+var work = { }
 
 work.position = "Asset Integrity Engineer";
 work.employer = "Oceaneering Asset Integrity";
 work.city =  "Trondheim, Norway";
 
 
-// create education object
+// format work variables in bracket format
+//    var formattedEmployer = HTMLemail.replace(["%data%"], work["employer"]);
+//    var formattedWorkTitle = HTMLworkTitle.replace(["%data%"], work["position"]);
+//    var formattedWorkLocation = HTMLworkLocation.replace(["%data%"], work["city"]);
+
+// append or prepend work variables
+//    $("#workExperience").append([formattedEmployer]);
+//    $("#workExperience").append([formattedWorkTitle]);
+//    $("#workExperience").append([formattedWorkLocation]);
 
 var education = {
   "schools" : [
@@ -113,27 +136,27 @@ var education = {
       "url": "#"
     }
   ]
-};
+}
 
+// education["name"] = "NTNU";
+// education["years"] = "Fall 2013 - Spring 2015";
+// education["city"] = work.city;
 
-// create projects array
+// format education variables in bracket format
+//    var formattedSchoolName = HTMLschoolName.replace(["%data%"], education.name);
+//    var formattedSchoolLocation = HTMLschoolLocation.replace(["%data%"], education.city);
+//    var formattedSchoolDates = HTMLschoolDates.replace(["%data%"], education.years);
 
-var projects = [
-  {
-    "title": "Tribute Page",
-    "date": "Jan 2017",
-    "description": "Free Code Camp Exercise",
-    "images": "#"
-  },
-  {
-    "title": "Tribute Page",
-    "date": "Jan 2017",
-    "description": "Free Code Camp Exercise",
-    "images": "#"
-  }
-];
+// append or prepend education variables
+//    $("#education").append([formattedSchoolName]);
+//    $("#education").append([formattedSchoolDates]);
+//    $("#education").append([formattedSchoolLocation]);
 
-// tests
-// $("#main").append([work["position"]]);
-// $("#main").append([education.schools[0].name]);
-// $("#main").append([projects[0].title]);
+var projects = {
+  ["title":"Tribute Page", "date":"Jan 2017", "description":"Free Code Camp Exercise", "images": ["#"]],
+  ["title":"Tribute Page", "date":"Jan 2017", "description":"Free Code Camp Exercise", "images": ["#"]],
+  ["title":"Tribute Page", "date":"Jan 2017", "description":"Free Code Camp Exercise", "images": ["#"]]
+}
+
+$("#main").append([work["position"]]);
+$("#main").append([education.schools[0].name]);
