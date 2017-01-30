7546f1c7014492d060a0f928304f7fe93de23210
marquesin <nmarquesin@live.com>
2017-01-30 16:08:01 +0000

    Add successfully changes from last lessons, except for the map.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -53,12 +53,55 @@ $("#header").append([formattedWelcomeMessage]);
 
 // create work object
 
-var work = { };
+var work = {
+  jobs : [{
+    "position" : "Asset Integrity Engineer",
+    "employer" : "Oceaneering Asset Integrity",
+    "dates" : "01.2011 - 01.2100",
+    "location" :  "Trondheim, Norway",
+    "descrip" : "Cupcake ipsum dolor sit amet chocolate bonbon lemon drops. Bear claw donut danish chocolate I love sweet wafer. Caramels wafer pudding cheesecake cupcake."
+  },
+  {
+    "position" : "Research Assistant",
+    "employer" : "NTNU",
+    "dates" : "01.2011 - 01.2100",
+    "location" :  "Trondheim, Norway",
+    "descrip" : "Cupcake ipsum dolor sit amet chocolate bonbon lemon drops. Bear claw donut danish chocolate I love sweet wafer. Caramels wafer pudding cheesecake cupcake."
+  }
+]};
 
-work.position = "Asset Integrity Engineer";
-work.employer = "Oceaneering Asset Integrity";
-work.city =  "Trondheim, Norway";
 
+
+function displayWork () {
+  for (item in work.jobs){
+    $("#workExperience").append([HTMLworkStart]);
+    var employer = HTMLworkEmployer.replace(["%data%"], work.jobs[item].employer);
+    var title = HTMLworkTitle.replace(["%data%"], work.jobs[item].position);
+    $(".work-entry:last").append([employer.concat(title)]);
+    var dates = HTMLworkDates.replace(["%data%"], work.jobs[item].dates);
+    var location = HTMLworkLocation.replace(["%data%"], work.jobs[item].location);
+    var description = HTMLworkDescription.replace(["%data%"], work.jobs[item].descrip);
+    $(".work-entry:last").append([dates]);
+    $(".work-entry:last").append([location]);
+    $(".work-entry:last").append([description]);
+  }
+};
+
+displayWork();
+
+//$(document).click(function(loc) {
+//  var x = loc.pageX;
+//  var y = loc.pageY;
+//
+//  logClicks(x,y);
+//});
+
+
+
+
+// work.position = "Asset Integrity Engineer";
+// work.employer = "Oceaneering Asset Integrity";
+// work.location =  "Trondheim, Norway";
 
 // create education object
 
@@ -84,31 +127,31 @@ var education = {
   "onlineCourses" : [
     {
       "title": "JavaScript Basics",
-      "school": "Udacity",
+      "school": "Udalocation",
       "dates": 2017,
       "url": "#"
     },
     {
       "title": "Intro to CS",
-      "school": "Udacity",
+      "school": "Udalocation",
       "dates": 2016,
       "url": "#"
     },
     {
       "title": "HTML and CSS Fundamentals",
-      "school": "Udacity",
+      "school": "Udalocation",
       "dates": 2017,
       "url": "#"
     },
     {
       "title": "Intro to Machine Learning",
-      "school": "Udacity",
+      "school": "Udalocation",
       "dates": 2017,
       "url": "#"
     },
     {
       "title": "Responsive Web Dev",
-      "school": "Udacity",
+      "school": "Udalocation",
       "dates": 2017,
       "url": "#"
     }
@@ -118,7 +161,8 @@ var education = {
 
 // create projects array
 
-var projects = [
+var projects = {
+  "projects" : [
   {
     "title": "Tribute Page",
     "date": "Jan 2017",
@@ -131,9 +175,53 @@ var projects = [
     "description": "Free Code Camp Exercise",
     "images": "#"
   }
-];
+]};
+
+projects.display = function() {
+  for (project in projects.projects) {
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedTitle);
+
+    var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].date);
+    $(".project-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedDescription);
+
+    if(projects.projects[project].images.length > 0) {
+      for (image in projects.projects[project].images) {
+        var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].image);
+        $(".project-entry:last").append(formattedImage);
+      }
+    }
+
+
+  }
+};
+
+projects.display();
 
 // tests
 // $("#main").append([work["position"]]);
 // $("#main").append([education.schools[0].name]);
 // $("#main").append([projects[0].title]);
+
+if (bio.skills) {
+  $("#header").append([HTMLskillsStart]);
+  $("#skills-h3").append([HTMLskills.replace(["%data%"], bio.skills)]);
+};
+
+function inName(name) {
+  name = name.trim().split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+  return name[0]+ " " + name[1];
+};
+
+$("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
